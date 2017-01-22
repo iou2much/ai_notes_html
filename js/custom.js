e62fa3c38bccfbe072df0323e65097b068a3f1a9
libo <iou2much@sina.com>
2017-01-22 15:08:45 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -12,20 +12,35 @@ $(document).ready(function() {
             $(this).addClass('en');
         }
     });
-    var title_idx=0;
+    var title_idx=[];
     $('body').append("<div class='toc'><ul id='toc'></ul></div>");
+
     var cur_node=$('#toc'),parr_node=$('#toc');
+
     $('h1,h2,h3,h4,h5,h6').each(function(){
         console.log(this);
 
 
-        if(title_idx%2==0){
+        if(title_idx.length%2==0){
             $(this).addClass('cn')
-            $('#toc').append('<li>'+this.innerText+'</li>');
+            // $('#toc').append('<li id="tt_'+title_idx.length+'">'+this.innerText+'</li>');
+            //parr_node=$('#tt_'+title_idx.length);
+
+            var lv = this.localName[1];
+
+            if(title_idx[title_idx.length-1].localName[1] == lv){
+                $(title_idx[title_idx.length-1]).parent().append('<li id="tt_'+title_idx.length+'">'+this.innerText+this.localName+'</li>');
+            }else if(title_idx[title_idx.length-1].localName[1] == lv-1){
+                $('#tt_'+title_idx.length-1).append('<li id="tt_'+title_idx.length+'">'+this.innerText+this.localName+'</li>');
+
+            }
         }else {
             $(this).addClass('en')
         }
-        title_idx++;
+
+
+
+        title_idx.push(this);
     })
 
 
