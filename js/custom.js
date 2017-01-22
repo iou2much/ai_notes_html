0182e44b398df033d88da3f706341341aeae4e19
libo <iou2much@sina.com>
2017-01-22 12:33:53 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     $('p').each(function(){
-        var txt = $(this).innerText;
+        var txt = this.innerText;
         var patt = /[\u4e00-\u9fa5]+/;
 
+        console.log(txt)
         if(patt.test(txt)){
             $(this).addClass('cn');
         }else{
