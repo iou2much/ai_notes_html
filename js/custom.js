018b952fc8ee7084b655d791c124afd6a5620072
libo <iou2much@sina.com>
2017-01-22 12:29:10 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
+    $('p').each(function(){
+        var txt = $(this).innerText;
+        var patt = /[\u4e00-\u9fa5]+/;
+
+        if(txt.test(patt)){
+            $(this).addClass('cn');
+        }else{
+            $(this).addClass('en');
+        }
+    })
 
 });
